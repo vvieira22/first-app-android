de34227c2e71456806bc972a17a5b53fe927af09
Vitor Vieira <vitorgoncalvesvieira22@gmail.com>
2022-12-15 15:44:52 -0300

    start register product view

--- app/src/main/java/com/example/mainactivity/CadastroProdutoActivity.kt
+++ app/src/main/java/com/example/mainactivity/CadastroProdutoActivity.kt
@@ -2,7 +2,32 @@ package com.example.mainactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class CadastroProdutoActivity :
     AppCompatActivity(R.layout.activity_cadastro_produto) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val botaoCadastro = findViewById<Button>(R.id.botaoCadastrar)
+                botaoCadastro.setOnClickListener {
+                    val nome = findViewById<EditText>(R.id.nomeProduto)
+                    val valorNome = nome.text.toString()
+
+                    val preco = findViewById<EditText>(R.id.precoCadastrado)
+                    val valorPreco = preco.text.toString()
+
+                    val descricao = findViewById<EditText>(R.id.descricaoProduto)
+                    val valorDescricao = descricao.text.toString()
+
+                    Log.i("janelaCadastro:", "Nome:$valorNome")
+                    Log.i("janelaCadastro:", "Preço:$valorPreco")
+                    Log.i("janelaCadastro:", "Descricao:$valorDescricao")
+                }
+    }
 }
